c059d91f675db4628f59b768694ca347bd37bee8
Asadulla56 <mdasadullahoque294@gmail.com>
2025-06-05 02:53:41 +0600

    code problem solve

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-
-import { BrowserRouter } from "react-router-dom";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <BrowserRouter basename="/Festivo-Event-Management">
-    </BrowserRouter>
-  );
+    <>
+      
+    </>
+  )
 }
 
-export default App;
+export default App
